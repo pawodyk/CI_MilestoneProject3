cb6ae4338e9a7277aead1352fd4bfff707cd1034
Pawel Wodyk <pawlosik@yahoo.com>
2019-06-15 20:05:45 +0000

    disabled the debug mode on the python app

--- run.py
+++ run.py
@@ -462,4 +462,4 @@ def delete_recipe_confirmed(recipe_id):
 if __name__ == "__main__": 
     app.run(host=os.environ.get('IP', '127.0.0.1'), 
             port=int(os.environ.get('PORT', 5000)), 
-            debug=True)
+            debug=False)
